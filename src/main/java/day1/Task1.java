57c1e021dd760bbe27c18e6d505e49921420445b
Sergey_Viktorovich <starwars228777@gmail.com>
2022-01-13 21:21:37 +0300

    day 1 - 4

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -1,7 +1,17 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        Scanner value = new Scanner(System.in);
+        int num = value.nextInt();
+
+        int a = 10;
+        while (a >= num) {
+            System.out.print(" JAVA №" + num);
+            num++;
+        }
     }
 }
