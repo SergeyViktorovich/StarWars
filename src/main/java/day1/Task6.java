46cabb6c4641e8834332f79404797737f3f92f8a
Sergey_Viktorovich <starwars228777@gmail.com>
2022-01-13 21:21:37 +0300

    day 1 - 4

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,18 @@
 package day1;
 
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
 public class Task6 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
 
+    int i = 0;
+        while (i <= 10) {
+            out.println(i + " x " + k + " = " + i * k);
+            i++;
+        }
     }
 }
--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,5 +1,7 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
